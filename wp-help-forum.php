67d27b04550e36e332f137b3e0401025a2f0cb29
Finn Bear <finnbearone@gmail.com>
2018-02-18 03:23:19 +0000

    Form working.

--- wp-help-forum.php
+++ wp-help-forum.php
@@ -35,13 +35,75 @@
 
    }
 
-   function help_forum_test() {
+   function help_forum_form() {
      $args = array();
 
-     echo 'Hello world';
+     echo '<form action"' . $_SERVER['REQUEST_URI'] . '" method="post">'
+?>
+	<div>
+		<label for="title"><strong>Title</strong></label>
+		<input type="text" name="title">
+	</div>
+	<div>
+		<label for="description"><strong>Description</strong></label>
+		<input type="text" name="description">
+	</div>
+	<br>
+	<input type="submit" name="submit" value="Submit">
+</form>
+<?php
+   }
+
+   function help_forum_form_handler() {
+     global $wpdb;
+
+     if ( isset( $_POST['submit'] ) ) {
+       $title = sanitize_text_field( $_POST['title'] );
+       $description = sanitize_text_field( $_POST['description'] );
+
+       $table_name = $wpdb->prefix . 'helpforum';
+
+       $wpdb->insert(
+         $table_name,
+         array(
+           'dateCreated' => current_time('mysql'),
+           'title' => $title,
+           'description' => $description,
+         )
+       );
+
+       echo '<p><strong>Posted.</strong></p>';
+       echo '<p> Title: ' . $title . '</p>';
+       echo '<p> Description: ' . $description . '</p>';
+     } else {
+       help_forum_form();
+     }
+   }
+
+   function help_forum_form_shortcode() {
+     ob_start();
+     help_forum_form_handler();
+     return ob_get_clean();
+   }
+
+   function help_forum_list() {
+
+   }
+
+   function help_forum_list_handler() {
+
+   }
+
+   function help_forum_list_shortcode() {
+     ob_start();
+     help_forum_list_handler();
+     return ob_get_clean();
    }
 
    register_activation_hook(__FILE__, 'help_forum_activate');
    register_deactivation_hook(__FILE__, 'help_forum_deactivate');
    register_uninstall_hook(__FILE__, 'help_forum_uninstall');
+
+   add_shortcode('help_forum_form', 'help_forum_form_shortcode');
+   add_shortcode('help_forum_list', 'help_forum_list_shortcode');
 ?>
