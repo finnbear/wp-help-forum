23554d7b078cf8aec754462a188a975c46e61f9d
Finn Bear <finnbearone@gmail.com>
2018-02-18 18:50:59 +0000

    Better address.

--- wp-help-forum.php
+++ wp-help-forum.php
@@ -26,9 +26,9 @@
        contactLastName VARCHAR(50) NOT NULL,
        contactEmail VARCHAR(100),
        contactPhone VARCHAR(20),
+       contactZip INTEGER,
        contactCity VARCHAR(50),
        contactState VARCHAR(2),
-       contactZip INTEGER,
        contactAddress VARCHAR(100),
        views INTEGER NOT NULL DEFAULT 0,
        PRIMARY KEY  (id)
@@ -56,27 +56,25 @@
 
      echo '<form action"' . $_SERVER['REQUEST_URI'] . '" method="post">';
      echo '<div>';
-     echo '<label for="need"><strong>What is needed?</strong></label>';
+     echo '<label for="need"><strong>What is needed? *</strong></label>';
      echo '<input type="text" name="need" maxlength="50" required>';
      echo '</div>';
      echo '<div>';
-     echo '<label for="beneficiary"><strong>Who is the beneficiary?</strong></label>';
+     echo '<label for="beneficiary"><strong>Who is the beneficiary? *</strong></label>';
      echo '<input type="text" name="beneficiary" maxlength="100" required>';
      echo '</div>';
      echo '<div>';
-     echo '<label for="circumstance"><strong>What are the circumstances (and other details)?</strong></label>';
+     echo '<label for="circumstance"><strong>What are the circumstances? *</strong></label>';
      echo '<textarea name="circumstance" maxlength="500" required></textarea>';
      echo '</div>';
      echo '<div>';
-     echo '<label for="contactName"><strong>Contact name</strong></label>';
+     echo '<label for="contactName"><strong>Contact name *</strong></label>';
      echo '<div name="contactName">';
      echo '<input style="width: 22%; margin-right: 1%;" type="text" name="contactTitle" maxlength="10" placeholder="Title" list="contactTitles" required>';
      echo '<datalist id="contactTitles">';
-     echo '<option value="Mr">';
-     echo '<option value="Mrs">';
-     echo '<option value="Ms">';
-     echo '<option value="Miss">';
-     echo '<option value="Mx">';
+     echo '<option value="Mr.">';
+     echo '<option value="Ms.">';
+     echo '<option value="Dr.">';
      echo '</datalist>';
      echo '<input style="width: 38%; margin-right: 1%;" type="text" name="contactFirstName" maxlength="50" placeholder="First" required>';
      echo '<input style="width: 38%;" type="text" name="contactLastName" maxlength="50" placeholder="Last" required>';
@@ -84,15 +82,72 @@
      echo '</div>';
      echo '<div>';
      echo '<label for="contactEmail"><strong>Contact email</strong></label>';
-     echo '<input type="email" name="contactEmail" maxlength="100" placeholder="Optional">';
+     echo '<input type="email" name="contactEmail" maxlength="100">';
      echo '</div>';
      echo '<div>';
      echo '<label for="contactPhone"><strong>Contact phone</strong></label>';
-     echo '<input type="tel" name="contactPhone" maxlength="20" placeholder="Optional">';
+     echo '<input type="tel" name="contactPhone" maxlength="20">';
      echo '</div>';
      echo '<div>';
      echo '<label for="contactAddress"><strong>Contact address</strong></label>';
-     echo '<input type="text" name="contactAddress" maxlength="100" placeholder="Optional">';
+     echo '<input style="margin-bottom: 5px;" type="text" name="contactAddress" maxlength="100" placeholder="Address">';
+     echo '<div>';
+     echo '<input style="width: 40%; margin-right: 1%;" type="text" name="contactCity" maxlength="50" placeholder="City">';
+     echo '<input style="width: 29%; margin-right: 1%;" type="text" name="contactState" maxlength="2" list="contactStates" placeholder="State">';
+     echo '<datalist id="contactStates">';
+     echo '<option value="AL">';
+     echo '<option value="AK">';
+     echo '<option value="AZ">';
+     echo '<option value="AR">';
+     echo '<option value="CA">';
+     echo '<option value="CO">';
+     echo '<option value="CT">';
+     echo '<option value="DE">';
+     echo '<option value="FL">';
+     echo '<option value="GA">';
+     echo '<option value="HI">';
+     echo '<option value="ID">';
+     echo '<option value="IL">';
+     echo '<option value="IN">';
+     echo '<option value="IA">';
+     echo '<option value="KS">';
+     echo '<option value="KY">';
+     echo '<option value="LA">';
+     echo '<option value="ME">';
+     echo '<option value="MD">';
+     echo '<option value="MA">';
+     echo '<option value="MI">';
+     echo '<option value="MN">';
+     echo '<option value="MO">';
+     echo '<option value="MT">';
+     echo '<option value="NE">';
+     echo '<option value="NV">';
+     echo '<option value="NH">';
+     echo '<option value="NJ">';
+     echo '<option value="NM">';
+     echo '<option value="NY">';
+     echo '<option value="NC">';
+     echo '<option value="ND">';
+     echo '<option value="OH">';
+     echo '<option value="OK">';
+     echo '<option value="OR">';
+     echo '<option value="PA">';
+     echo '<option value="RI">';
+     echo '<option value="SC">';
+     echo '<option value="SD">';
+     echo '<option value="TN">';
+     echo '<option value="TX">';
+     echo '<option value="UT">';
+     echo '<option value="VT">';
+     echo '<option value="VA">';
+     echo '<option value="WA">';
+     echo '<option value="WV">';
+     echo '<option value="WI">';
+     echo '<option value="WY">';
+     echo '</datalist>';
+     echo '<style>input[type=number] { -moz-appearance: textfield; } input[type=number]::-webkit-inner-spin-button, input[type=number]::-webkit-outer-spin-button { -webkit-appearance: none; margin: 0; }</style>';
+     echo '<input style="width: 29%;" type="number" name="contactZip" maxlength="10" placeholder="Zip">';
+     echo '</div>';
      echo '</div>';
      echo '<br>';
      echo '<input type="submit" name="submit" value="Submit">';
@@ -114,6 +169,9 @@
        $contactEmail = sanitize_email( $_POST['contactEmail'] );
        $contactPhone = sanitize_text_field( $_POST['contactPhone'] );
        $contactAddress = sanitize_text_field( $_POST['contactAddress'] );
+       $contactZip = sanitize_text_field( $_POST['contactZip'] );
+       $contactCity = sanitize_text_field( $_POST['contactCity'] );
+       $contactState = sanitize_text_field( strtoupper( $_POST['contactState'] ) );
 
        $table_name = $wpdb->prefix . 'helpforum';
 
@@ -130,6 +188,9 @@
            'contactEmail' => $contactEmail,
            'contactPhone' => $contactPhone,
            'contactAddress' => $contactAddress,
+           'contactZip' => $contactZip,
+           'contactCity' => $contactCity,
+           'contactState' => $contactState,
          )
        );
 
